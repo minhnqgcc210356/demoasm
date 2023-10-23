b066f773df14e26b7de6812e22463003db347166
minhnqgcc210356 <minhnqgcc210356@fpt.edu.vn>
2023-10-23 23:57:40 +0700

    new1

--- index.php
+++ index.php
@@ -15,6 +15,14 @@ $re=$dbLink->query($sql);
 
 if($re->num_rows>0){
 ?>
+<section class="py-5 text-center container"
+        style="background-image: url('https://media-api.advertisingvietnam.com/oapi/v1/media?uuid=f3f85054-a4a7-4bfe-a602-a7c1e61da085&type=image');height: 700px; width: 500vh" >
+        <div class="row py-ly-5">
+            <div class="col-lg-6 col-md-8 mx-auto">
+                <h1 class="fw-light colorblack">Toy Store</h1>
+            </div>
+        </div>
+    </section>
 <div class="container-fluid"></div>
 <div class="row">
 <?php
@@ -30,7 +38,7 @@ if($re->num_rows>0){
                     />
                     <div class="card-body"></div>
 
-                    <a href="#?proid=<?=$row['proid']?>" class="text-decoration-none text-center"><h5 
+                    <a href="detail.php?proid=<?=$row['proid']?>" class="text-decoration-none text-center"><h5 
                      class="card-title"><?=$row['name']?></h5></a>
                     <h6 class="card-subtitle mb-2 text-center"><span></span><?=$row['price']?></h6>
                     <a href="detail.php?proid=<?=$row['proid']?>" class="btn btn-primary"><h8>More detail...</h8></a>
