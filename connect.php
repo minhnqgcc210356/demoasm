14b2de260bb45d52ec45effd6255fe02a025b0ce
minhnqgcc210356 <minhnqgcc210356@fpt.edu.vn>
2023-10-23 22:14:31 +0700

    new1

--- connect.php
+++ connect.php
@@ -8,7 +8,7 @@ class Connect{
     {
         $this->server   ="uzb4o9e2oe257glt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
         $this->username ="gpfkkk7dv906okev";
-        $this->password ="whdh7li8ay2j8zcu	
+        $this->password ="iglqx01z3979q8b1	
         ";
         $this->dbName   ="n67aepjbzgvyqgxd";
     }
@@ -28,7 +28,9 @@ class Connect{
     //option2 :PDO
     function connectToPDO():PDO{
         try{
-            $conn = new PDO("mysql:host=$this->server;dbname=$this->dbName",$this->username,$this->password);
+            $conn = new PDO("mysql://gpfkkk7dv906okev:iglqx01z3979q8b1@uzb4o9e2oe257glt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/n67aepjbzgvyqgxd
+
+        );
             
         }catch(PDOException $e){
             die("Falied " .$e);
