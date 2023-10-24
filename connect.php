1e416e6cd529c00625615fa783a9b5ae5a623f08
minhnqgcc210356 <minhnqgcc210356@fpt.edu.vn>
2023-10-24 14:13:01 +0700

    new1

--- connect.php
+++ connect.php
@@ -6,16 +6,16 @@ class Connect{
     public $password;
     public function __construct()
     {
-        $this->server   ="uzb4o9e2oe257glt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+        $this->server   ="uzb4o9e2oe257glt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	";
         $this->username ="gpfkkk7dv906okev";
-        $this->password ="iglqx01z3979q8b1";
+        $this->password ="pnzdlr6jytnwz5n3";
         $this->dbName   ="n67aepjbzgvyqgxd";
     }
 
     // Option 1:mysqli
     function connectToMySQL():mysqli{
-    $conn = new mysqli("mysql://gpfkkk7dv906okev:iglqx01z3979q8b1@uzb4o9e2oe257glt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/n67aepjbzgvyqgxd
-    ");
+    $conn = new mysqli($this->server,
+    $this->username, $this->password, $this->dbName);
     
     if($conn->connect_error){
         die("Failed ".$conn->connect_error);
